8eb7e8b9a6a25beac62fc5d9b119807555b957c6
Eelis Holmsten <holmsteneelis@gmail.com>
2024-03-16 12:55:43 +0000

    basic clap cli

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,58 @@
-use std::env;
+use clap::Parser;
+use clap_derive::Parser;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use wallet_gen::coin::Coin;
 use wallet_gen::ethereum::new_wallet;
 use wallet_gen::wallet::Wallet;
 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Match address start
+    #[arg(long)]
+    start: Option<String>,
+
+    /// Match any position
+    #[arg(long)]
+    fill: Option<char>,
+
+    /// Match wallet end
+    #[arg(long)]
+    end: Option<String>,
+
+    /// How many threads to use
+    #[arg(long)]
+    threads: Option<u16>,
+}
+
 fn main() {
-    let mut args = env::args();
-    let end = args.nth_back(0).unwrap();
-    let fill = args.nth_back(0).unwrap().chars().nth(0).unwrap();
-    let start = args.nth_back(0).unwrap();
+    let cli = Cli::parse();
+
+    let start = match cli.start {
+        Some(s) => s,
+        None => "".to_string(),
+    };
+
+    let fill = match cli.fill {
+        Some(s) => s,
+        None => ' ',
+    };
+
+    let end = match cli.end {
+        Some(s) => s,
+        None => "".to_string(),
+    };
+
+    let threads = match cli.threads {
+        Some(t) => t,
+        None => 1,
+    };
 
     let best_wallet = Arc::new(Mutex::new((new_wallet(Coin::Ethereum).unwrap(), 0, 0, 0)));
     let mut handles = vec![];
 
-    for _ in 0..8 {
+    for _ in 0..threads {
         let best_wallet_clone = Arc::clone(&best_wallet);
         let start = start.clone();
         let end = end.clone();
